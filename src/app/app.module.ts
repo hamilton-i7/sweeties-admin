50d344def29006dea45d660f0d208782aaa80712
Juan Hamilton <hedwards507@gmail.com>
2023-03-09 17:12:37 -0500

    Setup Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FeaturesModule } from './features/features.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideStorage, getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FeaturesModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FeaturesModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'sweeties672-4f600',
+    appId: '1:774831250273:web:0d42929575249384fa0320',
+    storageBucket: 'sweeties672-4f600.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAkV3WOeVsvHg3pDv1DH7eydbAmsA3jb7E',
+    authDomain: 'sweeties672-4f600.firebaseapp.com',
+    messagingSenderId: '774831250273',
+    measurementId: 'G-38JVB6VB12',
+  },};
