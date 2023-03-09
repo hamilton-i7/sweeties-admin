146e808cdcdb16fdb7b0b493988075ddde2ac9f9
Juan Hamilton <hedwards507@gmail.com>
2023-03-08 23:35:46 -0500

    Add Clients component

---
+++ src/app/features/home/components/client-item/client-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-item',
+  templateUrl: './client-item.component.html',
+  styleUrls: ['./client-item.component.scss']
+})
+export class ClientItemComponent {
+
+}
--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { ShareModule } from '../../share/share.module';
 import { HomeTopBarComponent } from './components/home-top-bar/home-top-bar.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { ClientItemComponent } from './components/client-item/client-item.component';
 
 @NgModule({
-  declarations: [HomeComponent, HomeTopBarComponent],
+  declarations: [HomeComponent, HomeTopBarComponent, ClientsComponent, ClientItemComponent],
   imports: [CommonModule, ShareModule],
   exports: [HomeComponent],
 })
