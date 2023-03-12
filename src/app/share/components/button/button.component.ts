8745a0a69a36d7fb01e44f2d1356df538d936c55
Juan Hamilton <hedwards507@gmail.com>
2023-03-11 19:23:39 -0500

    Add disabled state to ButtonComponent

--- src/app/share/components/button/button.component.ts
+++ src/app/share/components/button/button.component.ts
@@ -1,41 +1,22 @@
-import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
 })
-export class ButtonComponent implements OnInit {
+export class ButtonComponent {
+  public buttonVariant: typeof ButtonVariant = ButtonVariant;
+
   @Input() label = '';
   @Output() btnClick = new EventEmitter();
   @Input() variant: ButtonVariant = ButtonVariant.FILLED;
-  variantClass = '';
-
+  @Input() disabled = false;
   @Input() href?: string;
 
-  ngOnInit(): void {
-    this.setupVariantClass();
-  }
-
   onClick(): void {
+    if (this.disabled) return;
     this.btnClick.emit();
-  }
-
-  setupVariantClass(): void {
-    switch (this.variant) {
-      case ButtonVariant.FILLED:
-        this.variantClass = 'button--filled';
-        break;
-      case ButtonVariant.TONAL:
-        this.variantClass = 'button--tonal';
-        break;
-      case ButtonVariant.OUTLINED:
-        this.variantClass = 'button--outlined';
-        break;
-      case ButtonVariant.TEXT:
-        this.variantClass = 'button--text';
-        break;
-    }
   }
 }
 
