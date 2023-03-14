e20fe64469096671eb2417edf2e094a2248bafb1
Juan Hamilton <hedwards507@gmail.com>
2023-03-14 18:14:55 -0500

    Protect routes

--- src/app/features/menu/menu-routing.module.ts
+++ src/app/features/menu/menu-routing.module.ts
@@ -3,27 +3,37 @@ import { RouterModule, Routes } from '@angular/router';
 import { AddEditCategoryComponent } from './pages/add-edit-category/add-edit-category.component';
 import { AddEditProductComponent } from './pages/add-edit-product/add-edit-product.component';
 import { MenuComponent } from './pages/menu/menu.component';
+import { canActivate, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
 
 const routes: Routes = [
   {
     path: 'menu/category/:id',
     component: AddEditCategoryComponent,
+    ...canActivate(() => redirectUnauthorizedTo(['/login'])),
   },
   {
     path: 'menu/add-category',
     component: AddEditCategoryComponent,
     title: 'Agregar Categoría',
+    ...canActivate(() => redirectUnauthorizedTo(['/login'])),
   },
   {
     path: 'menu/add-product',
     component: AddEditProductComponent,
     title: 'Agregar Producto',
+    ...canActivate(() => redirectUnauthorizedTo(['/login'])),
   },
   {
     path: 'menu/product/:id',
     component: AddEditProductComponent,
+    ...canActivate(() => redirectUnauthorizedTo(['/login'])),
   },
-  { path: 'menu', component: MenuComponent, title: 'Menú' },
+  {
+    path: 'menu',
+    component: MenuComponent,
+    title: 'Menú',
+    ...canActivate(() => redirectUnauthorizedTo(['/login'])),
+  },
 ];
 
 @NgModule({
