69e942aa94100aba77791ebb0415b6a134e0aa51
Juan Hamilton <hedwards507@gmail.com>
2023-03-20 15:13:40 -0500

    Improve disabled state on text fields

--- src/app/features/users/user-routing.module.ts
+++ src/app/features/users/user-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
+import { canActivate, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './pages/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'users/profile',
+    component: ProfileComponent,
+    ...canActivate(() => redirectUnauthorizedTo(['/login'])),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class UserRoutingModule { }
+export class UserRoutingModule {}
